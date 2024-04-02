1dc25a92ad8b62c66acb726bf444ce02c96e4ec7
Ivan Maksimov <ivan_kratovo@hotmail.com>
2024-04-02 13:34:33 +0200

    backend more

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const {
+  createProduct,
+  getaProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+  addToWishlist,
+  rating,
+  uploadImages,
+} = require("../controller/productCtrl");
+
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const {
+  uploadPhoto,
+  productImgResize,
+} = require("../middlewares/uploadImages");
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createProduct);
+router.put(
+  "/upload/:id",
+  authMiddleware,
+  isAdmin,
+  uploadPhoto.array("images", 10),
+  productImgResize,
+  uploadImages
+);
+router.get("/:id", getaProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, rating);
+
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
+
+router.get("/", getAllProducts);
+
+module.exports = router;
